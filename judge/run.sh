41700f4e320756848b08e0b0bf4c3c57ba6e43d9
WGQZYJZ <1074781857@qq.com>
2023-12-10 19:44:58 +0800

    Update run.sh

--- judge/run.sh
+++ judge/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run --rm -it \
-    -v "D:\桌面\软分\第二次大作业\lab2-handout\judge/global:/mnt/global_space" -v "D:\桌面\软分\第二次大作业\lab2-handout\judge/user:/mnt/user_space" \
+    -v ./global:/mnt/global_space -v ./user:/mnt/user_space \
     --cpus 4 --pids-limit 512 --memory 8g --network none --memory-swap -1 \
     --hostname container --add-host="container:127.0.0.1" \
     -e TOKEN=1:submission \
