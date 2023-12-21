c9edb82c1c2abc7d3daeec98b5e8c44f6cba2eaf
WGQZYJZ <1074781857@qq.com>
2023-12-21 22:30:28 +0800

    成功在本地评测

--- judge/run.sh
+++ judge/run.sh
@@ -5,3 +5,5 @@ docker run --rm -it \
     --hostname container --add-host="container:127.0.0.1" \
     -e TOKEN=1:submission \
     lab2_submitter_local
+
+docker run --rm -it -v "D:\Github\SyGusSolver\judge/global:/mnt/global_space" -v "D:\Github\SyGusSolver\judge/user:/mnt/user_space" --cpus 4 --pids-limit 512 --memory 8g --network none --memory-swap -1 --hostname container --add-host="container:127.0.0.1" -e TOKEN=1:submission lab2_submitter_local
